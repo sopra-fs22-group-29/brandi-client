8b28db87fa463962678bb9da944157627e86d77f
Eric Leger <33186220+EriCreator@users.noreply.github.com>
2022-04-04 14:20:03 +0200

    feat(): Create setup for ThreeJS (#20)
    Create /test url to test 3D stuff.
    Drag and drop our model to https://gltf.pmnd.rs/, which converts it into different models.
    Remove React logo, that constantly changes width and height of the viewport.
    Create two examples to test ThreeJS:
     1. An example with two Boxes that you can interact with.
     2. An import of our Brändy Dog model.
    
    Update React version in order to use use threejs.
    
    dependencies:
     - update react 17.0.2 -> 18.0.0
     - install three and @react-three/fiber
     - install @react-three/drei, in order to load GLTF models

--- src/components/routing/routers/AppRouter.js
+++ src/components/routing/routers/AppRouter.js
@@ -1,8 +1,9 @@
-import {BrowserRouter, Redirect, Route, Switch} from "react-router-dom";
-import {GameGuard} from "components/routing/routeProtectors/GameGuard";
+import { GameGuard } from "components/routing/routeProtectors/GameGuard";
+import { LoginGuard } from "components/routing/routeProtectors/LoginGuard";
 import GameRouter from "components/routing/routers/GameRouter";
-import {LoginGuard} from "components/routing/routeProtectors/LoginGuard";
 import Login from "components/views/Login";
+import Test from "components/views/Test";
+import { BrowserRouter, Redirect, Route, Switch } from "react-router-dom";
 
 /**
  * Main router of your application.
@@ -19,16 +20,19 @@ const AppRouter = () => {
       <Switch>
         <Route path="/game">
           <GameGuard>
-            <GameRouter base="/game"/>
+            <GameRouter base="/game" />
           </GameGuard>
         </Route>
         <Route exact path="/login">
           <LoginGuard>
-            <Login/>
+            <Login />
           </LoginGuard>
         </Route>
         <Route exact path="/">
-          <Redirect to="/game"/>
+          <Redirect to="/game" />
+        </Route>
+        <Route exact path="/test">
+          <Test />
         </Route>
       </Switch>
     </BrowserRouter>
@@ -36,6 +40,6 @@ const AppRouter = () => {
 };
 
 /*
-* Don't forget to export your component!
+ * Don't forget to export your component!
  */
 export default AppRouter;
