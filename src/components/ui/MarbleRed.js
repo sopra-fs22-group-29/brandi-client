46a9395b9dbadf855717688b9ef8da94fd472589
Elena Graf <Elena.Graf@uzh.ch>
2022-04-11 15:06:22 +0200

    Modeled Marbles & initial set up on board

---
+++ src/components/ui/MarbleRed.js
@@ -0,0 +1,18 @@
+import React, { useRef } from "react";
+import { useGLTF } from "@react-three/drei";
+
+export const MarbleRed = (props) => {
+  const group = useRef();
+  const { nodes, materials } = useGLTF("gltf/marble_red.gltf");
+  return (
+    <group ref={group} {...props} dispose={null}>
+      <mesh
+        geometry={nodes.Sphere.geometry}
+        material={materials.Rot}
+        scale={0.09}
+      />
+    </group>
+  );
+};
+
+useGLTF.preload("gltf/Kugel_Rot.gltf");
---
+++ src/components/ui/MarbleYellow.js
@@ -0,0 +1,18 @@
+import React, { useRef } from "react";
+import { useGLTF } from "@react-three/drei";
+
+export const MarbleYellow = (props) => {
+  const group = useRef();
+  const { nodes, materials } = useGLTF("gltf/marble_yellow.gltf");
+  return (
+    <group ref={group} {...props} dispose={null}>
+      <mesh
+        geometry={nodes.Sphere.geometry}
+        material={materials.Gelb}
+        scale={0.09}
+      />
+    </group>
+  );
+};
+
+useGLTF.preload("gltf/Kugel_Gelb.gltf");
