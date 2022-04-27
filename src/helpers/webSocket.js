f7e7654a364d515d90521ccdbac338e23bcf7a8d
Eric Leger <33186220+EriCreator@users.noreply.github.com>
2022-04-27 16:47:27 +0200

    feat(): Finish websocket setup (#44)
    We do now have the ability to notify all players inside of a game.

--- src/helpers/webSocket.js
+++ src/helpers/webSocket.js
@@ -4,9 +4,10 @@ import { userAuthData } from "./authentification";
 import { getDomain } from "./getDomain";
 
 var stompClient = null;
+var sessionId = "";
 
 export const connect = async (gameLink) => {
-  const url = getDomain() + "/connected";
+  const url = getDomain() + "/websocket";
   var socket = new SockJS(url);
   stompClient = over(socket);
   const authData = userAuthData();
@@ -17,25 +18,41 @@ export const connect = async (gameLink) => {
     },
     function (frame) {
       console.log("WebSocket Connected " + frame);
+      setSessionIdFromURL(stompClient.ws._transport.url);
 
       // subscribe to all the routes that we want te be notified from
-      // (we could use /client/COLOR in order to notify only one player)
-      stompClient.subscribe("/client/move", function (messageOutput) {
-        console.log("player moved:" + messageOutput.body);
-      });
-      stompClient.subscribe("/client/connected", function (messageOutput) {
-        console.log("player connected: " + messageOutput);
-      });
+      // // (we could use /client/COLOR in order to notify only one player)
+      // stompClient.subscribe("/client/move", function (messageOutput) {
+      //   console.log("player moved:" + messageOutput.body);
+      // });
+      // stompClient.subscribe("/client/connected", function (messageOutput) {
+      //   console.log("player connected: " + messageOutput);
+      // });
       stompClient.subscribe(
-        "/client/connected/" + gameLink,
+        "/client/test" + "-user" + sessionId,
         function (messageOutput) {
-          console.log("player connected to room: " + messageOutput);
+          console.log("test: " + messageOutput);
         }
       );
+      // stompClient.subscribe(
+      //   "/client/connected/{room}",
+      //   function (messageOutput) {
+      //     console.log("{room}: " + messageOutput);
+      //   }
+      // );
+      // stompClient.subscribe(
+      //   "/client/connected/" + gameLink,
+      //   function (messageOutput) {
+      //     console.log(
+      //       "***************player connected to room: " + messageOutput
+      //     );
+      //   }
+      // );
 
       // send initial message to notify everyone that we have successfully connected
-      notifyConnected();
-      greet(gameLink);
+      // notifyConnected();
+      // greet(gameLink);
+      test(gameLink);
     }
   );
 };
@@ -46,36 +63,52 @@ export const disconnect = () => {
   }
 };
 
-export const joinRoom = (roomId) => {
-  connect(roomId);
-  console.log("-----I went here-------");
-  stompClient.subscribe("/room/" + roomId, function (greeting) {
-    alert("Yey something happened !", greeting);
-  });
-};
+// export const joinRoom = (roomId) => {
+//   connect(roomId);
+//   console.log("-----I went here-------");
+//   stompClient.subscribe("/room/" + roomId, function (greeting) {
+//     alert("Yey something happened !", greeting);
+//   });
+// };
 
 // below are functions to send information to the websocket server
 
-export const greet = (roomId) => {
-  stompClient.send("/client/connected/" + roomId);
+// export const greet = (roomId) => {
+//   stompClient.send("/app/websocket/" + roomId + "/connect");
+// };
+
+// export const notifyConnected = () => {
+//   stompClient.send("/app/websocket/connected", {}, null);
+// };
+
+export const test = (roomId) => {
+  stompClient.send("/app/websocket/" + roomId + "/test");
 };
 
-export const notifyConnected = () => {
-  stompClient.send("/app/connected", {}, null);
-};
+// export const executeExampleMove = () => {
+//   stompClient.send(
+//     "/app/websocket/move",
+//     {},
+//     JSON.stringify({
+//       ballId: 1,
+//       destinationTile: 22,
+//       card: {
+//         id: 333,
+//         rank: "SEVEN",
+//         suit: "DIAMOND",
+//       },
+//     })
+//   );
+// };
 
-export const executeExampleMove = () => {
-  stompClient.send(
-    "/app/move",
-    {},
-    JSON.stringify({
-      ballId: 1,
-      destinationTile: 22,
-      card: {
-        id: 333,
-        rank: "SEVEN",
-        suit: "DIAMOND",
-      },
-    })
+const setSessionIdFromURL = (url) => {
+  url = url.replace("ws://localhost:8080/websocket/", "");
+  url = url.replace(
+    "ws://https://sopra-fs22-group-29-server.herokuapp.com/connected/",
+    ""
   );
+  url = url.replace("/websocket", "");
+  url = url.replace(/^[0-9]+\//, "");
+  console.log("Your current session is: " + url);
+  sessionId = url;
 };
