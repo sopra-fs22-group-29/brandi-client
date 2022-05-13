c8f153e98b8b908fddcec2fd02d5350ea3a002c8
Eric Léger <leger.eric@hotmail.com>
2022-05-14 01:22:57 +0200

    feat(#86, #63): Jump animation for marble and removing marble animation
    Implement a basic animation when marbles are moving.
    Basic animation to remove a marble after moving (when we land on another marble).
    
    closes #86, #63

--- src/helpers/animations.js
+++ src/helpers/animations.js
@@ -52,6 +52,72 @@ export const marbleJump = async (ref, coordinates, destinationHole) => {
   animationRunning = false;
 };
 
+export const marbleMove = async (ref, holesArray) => {
+  await waitForAnimationToFinish();
+  animationRunning = true;
+
+  let duration = 0;
+  if (holesArray.length === 2) {
+    duration = 100;
+  } else if (holesArray.length < 9) {
+    duration = 80;
+  } else {
+    duration = 70;
+  }
+
+  document.body.style.cursor = "auto";
+
+  for (let i = 0; i < holesArray.length - 1; i++) {
+    await jumpOneHole(ref, holesArray[i], holesArray[i + 1], duration);
+  }
+
+  animationRunning = false;
+};
+
+const jumpOneHole = async (ref, from, to, duration) => {
+  const fromCoordinates = marblePosition(from);
+  const toCoordinates = marblePosition(to);
+  const movement = [
+    toCoordinates[0] - fromCoordinates[0],
+    toCoordinates[1] - fromCoordinates[1],
+    toCoordinates[2] - fromCoordinates[2],
+  ];
+  // console.log("from hole: " + from + " " + fromCoordinates);
+  // console.log("to hole: " + to + " " + toCoordinates);
+  // console.log(movement);
+  const loopDelay = 2;
+  const iterations = duration / loopDelay;
+  const yMovement = Math.PI / iterations;
+  const height = 0.025;
+
+  const movementPerIteration = [
+    movement[0] / iterations,
+    movement[1] / iterations,
+    movement[2] / iterations,
+  ];
+  //   console.log(movementPerIteration);
+
+  for (let i = 0; i < iterations; i++) {
+    ref.current.position.x = fromCoordinates[0] + i * movementPerIteration[0];
+    ref.current.position.y =
+      fromCoordinates[1] + Math.sin(yMovement * i) * height;
+    ref.current.position.z = fromCoordinates[2] + i * movementPerIteration[2];
+
+    // if (i % 30 == 0) {
+    //   console.log("coordinates: ");
+    //   console.log(ref.current.position.x);
+    //   console.log(ref.current.position.y);
+    //   console.log(ref.current.position.z);
+    // }
+
+    await waitForMs(loopDelay);
+  }
+
+  ref.current.position.x = toCoordinates[0];
+  ref.current.position.y = toCoordinates[1];
+  ref.current.position.z = toCoordinates[2];
+};
+
 export const waitForAnimationToFinish = async () => {
   while (animationRunning) {
     await waitForMs(2);
